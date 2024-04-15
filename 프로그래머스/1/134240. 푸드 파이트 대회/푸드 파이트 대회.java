954c4cd7cc2c8bb97a05a9dfd985b0de155634f4
sanikani <111363766+sanikani@users.noreply.github.com>
2024-04-15 21:01:21 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.85 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] food){
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<food.length;i++){
+            for(int j=0;j<food[i]/2;j++){
+                sb.append(i);
+            }
+        }
+        String answer = sb.toString();
+        answer += "0";
+        answer += sb.reverse().toString();
+        return answer;
+    }
+}
