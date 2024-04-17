97322e1eb599e81c60de70f664a75af2762c6cb2
sanikani <111363766+sanikani@users.noreply.github.com>
2024-04-17 09:14:10 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -3,21 +3,9 @@ class Solution {
         int a = 0;
         int b = 0;
         for(String x : goal){
-            if(a<cards1.length && b<cards2.length){
-                System.out.println(cards1[a]+" "+cards2[b]+" "+x);
-                if(cards1[a].equals(x)) a++;
-                else if(cards2[b].equals(x)) b++;
-                else return "No";
-            }else if(a>=cards1.length){
-                System.out.println(cards2[b]+" "+x);
-                if(cards2[b].equals(x)) b++;
-                else return "No";
-            }else if(b>=cards2.length){
-                System.out.println(cards1[a]+" "+x);
-                if(cards1[a].equals(x)) a++;
-                else return "No";
-            }
-            
+            if(a<cards1.length && cards1[a].equals(x)) a++;
+            else if(b<cards2.length && cards2[b].equals(x)) b++;
+            else return "No";
         }
         return "Yes";
     }
