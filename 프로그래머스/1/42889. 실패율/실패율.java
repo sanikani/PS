886bdd97ab206dd411ca35ee3f8e8bb7a982cf8b
sanikani <111363766+sanikani@users.noreply.github.com>
2024-05-14 18:52:40 +0900

    [level 1] Title: 실패율, Time: 1.05 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        
+        int[] answer = new int[N];
+        int total = stages.length;
+        Arrays.sort(stages);
+        List<Rate> list = new ArrayList<>();
+        
+        for(int i=0;i<N;i++){
+            list.add(new Rate(i+1,0));
+        }
+        
+        for(int i=0;i<stages.length-1;i++){
+            if(stages[i]==N+1) break;
+            int length = stages.length - i;
+            int cnt = 1;
+            
+            for(int j=i;j<stages.length-1;j++){
+                if(stages[j]!=stages[j+1]) break;
+                cnt++;
+            }
+            
+            float fail = (float)cnt/length;
+            list.get(stages[i]-1).fail = fail;
+            i = i + cnt-1;
+        }
+        
+        Collections.sort(list);
+        int idx = 0;
+        
+        for(Rate r : list){
+            answer[idx++] = r.stage;
+        }
+        
+        return answer;
+    }
+    
+    public static class Rate implements Comparable<Rate>{
+        
+        int stage;
+        float fail;
+        
+        public Rate(int s, float f){
+            stage = s;
+            fail = f;
+        }
+        
+        @Override
+        public int compareTo(Rate rate){
+            if(this.fail == rate.fail){
+                return this.stage-rate.stage;
+            }
+            
+            if(this.fail>rate.fail) return -1;
+            else return 1;
+        }
+    }
+}
