9a81de7eb46c246f4725d6cf9a7cb5dd49128277
sanikani <111363766+sanikani@users.noreply.github.com>
2024-05-16 20:54:39 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.09 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    public int solution(String dartResult) {
+        dartResult = dartResult.replace("10","K").replace("S","1").replace("D","2").replace("T","3");
+        double[] scores = new double[100];
+        int index = 0;
+        for(int i=0;i<dartResult.length();i++){
+            char cur = dartResult.charAt(i);
+            if(Character.isDigit(cur)||cur=='K'){
+                int x = cur-'0';
+                if(cur == 'K') x = 10;
+                int y = dartResult.charAt(++i) - '0';
+                scores[index++] = Math.pow(x,y);
+            }else{
+                if(cur=='*'){
+                    if(index == 1){
+                        scores[index-1] = scores[index-1]*2;
+                    }else{
+                        scores[index-1] = scores[index-1]*2;
+                        scores[index-2] = scores[index-2]*2;
+                        // System.out.println("index: "+(index-2)+"에 "+scores.get(index-2)*2+"추가");
+                    }
+                }else{
+                   //scores.add(index,scores.get(index)*(-1));
+                    scores[index-1] = scores[index-1]*(-1);
+                }
+            }
+        }
+        int answer = 0;
+        
+        for(double x : scores){
+            answer += x;
+        }
+        
+        return answer;
+    }
+}
