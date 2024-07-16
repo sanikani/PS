fbacbbbc183a100c5ce11f03d44643cb1bc295c8
sanikani <111363766+sanikani@users.noreply.github.com>
2024-07-16 20:52:17 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 12.12 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -7,30 +7,38 @@ class Solution {
         for(int[] d : data){
             list.add(new Data(d[0],d[1],d[2],d[3]));
         }
-        list = list.stream().filter(d -> d.getXxx(ext)<val_ext).collect(Collectors.toList());
-        System.out.println(list.size());
-        list.sort(Comparator.comparing(d->d.getXxx(sort_by)));
+        
+        list = list.stream()
+            .filter(d -> d.getXxx(ext)<val_ext)
+            .sorted(Comparator.comparing(d->d.getXxx(sort_by)))
+            .collect(Collectors.toList());
+        
         int[][] answer = new int[list.size()][4];
+        
         for(int i=0;i<list.size();i++){
             answer[i][0] = list.get(i).code;
             answer[i][1] = list.get(i).date;
             answer[i][2] = list.get(i).maximum;
             answer[i][3] = list.get(i).remain;
-            
         }
+        
         return answer;
     }
+    
     class Data{
+        
         int code;
         int date;
         int maximum;
         int remain;
+        
         public Data(int code, int date, int maximum, int remain){
             this.code = code;
             this.date = date;
             this.maximum = maximum;
             this.remain = remain;
         }
+        
         public int getXxx(String s){
             if(s.equals("code")){
                 return code;
@@ -42,5 +50,6 @@ class Solution {
                 return remain;
             }
         }
+        
     }
 }
