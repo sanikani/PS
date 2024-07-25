2691e6210e92b99632409dea80e29095b35b8dd1
sanikani <111363766+sanikani@users.noreply.github.com>
2024-07-25 19:09:58 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 17.70 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,29 +1,40 @@
 import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        StringBuilder sb = new StringBuilder();
-        Map<String,Integer> map = new HashMap<>();
-        String[] keySet = {"R","T","C","F","J","M","A","N"};
-        for(String key : keySet) map.put(key,0);
+        Map<Character, Integer> scores = IntStream.range(0, survey.length)
+            .boxed()
+            .flatMap(i -> {
+                char type1 = survey[i].charAt(0);
+                char type2 = survey[i].charAt(1);
+                int score = choices[i] - 4;
+                if (score < 0) {
+                    return Stream.of(new AbstractMap.SimpleEntry<>(type1, -score));
+                } else if (score > 0) {
+                    return Stream.of(new AbstractMap.SimpleEntry<>(type2, score));
+                }
+                return Stream.empty();
+            })
+            .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)));
 
-        for(int i=0;i<survey.length;i++){
-            String[] tmp = survey[i].split("");
-            if(choices[i]==1||choices[i]==2||choices[i]==3){
-                map.put(tmp[0],map.get(tmp[0])+Math.abs(choices[i]-4));
-            }else if(choices[i]==4) continue;
-            else{
-                map.put(tmp[1],map.get(tmp[1])+choices[i]-4);
-            }
+        return "RCJA".chars()
+            .mapToObj(c -> (char) c)
+            .map(c -> {
+                char opposite = getOpposite(c);
+                return scores.getOrDefault(c, 0) >= scores.getOrDefault(opposite, 0) ? c : opposite;
+            })
+            .map(String::valueOf)
+            .collect(Collectors.joining());
+    }
+
+    private char getOpposite(char c) {
+        switch (c) {
+            case 'R': return 'T';
+            case 'C': return 'F';
+            case 'J': return 'M';
+            case 'A': return 'N';
+            default: throw new IllegalArgumentException("Invalid character: " + c);
         }
-        System.out.println(map);
-        for(int i=0;i<8;i+=2){
-            if(map.get(keySet[i])>map.get(keySet[i+1])) sb.append(keySet[i]);
-            else if(map.get(keySet[i])<map.get(keySet[i+1])) sb.append(keySet[i+1]);
-            else{
-                if(keySet[i].compareTo(keySet[i+1])>0) sb.append(keySet[i+1]);
-                else sb.append(keySet[i]);
-            }
-        }
-        return sb.toString();
     }
 }
