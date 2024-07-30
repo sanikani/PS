d0355532a07ec3bf5abd7ad6233cf0c2557f4154
sanikani <111363766+sanikani@users.noreply.github.com>
2024-07-30 18:05:57 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.05 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -5,17 +5,13 @@ class Solution {
         for(int i=0;i<attacks.length;i++){
             
             health += (attacks[i][0]-time-1)*bandage[1];
-            System.out.println((attacks[i][0]-time-1)*bandage[1]+" 회복");
             if(attacks[i][0]-time>bandage[0]){
                 health += ((attacks[i][0]-time-1)/bandage[0])*bandage[2];
-                System.out.println(((attacks[i][0]-time-1)/bandage[0])*bandage[2]+" 추가 회복");
             }
             if(health>max) health = max;
             health -= attacks[i][1];
-            System.out.println(attacks[i][1]+" 공격");
             if(health<=0) return -1;
             time = attacks[i][0];
-            System.out.println(health+"현재체력, 시간"+time);
         }
         
         return health;
