b80bdd9304bb8f9ec4061b112aba1b6dcaaeb6f2
sanikani <111363766+sanikani@users.noreply.github.com>
2024-12-24 16:38:43 +0900

    [level 2] Title: 전화번호 목록, Time: 290.15 ms, Memory: 223 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,10 +2,16 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
+        Map<String,Integer> map = new HashMap<>();
+        for(String s : phone_book){
+            map.put(s,0);
+        }
+        
+        for(int i=0;i<phone_book.length;i++){
+            for(int j=0;j<phone_book[i].length();j++){
+                if(map.containsKey(phone_book[i].substring(0,j))){
+                    return false;
+                }
             }
         }
         
