9e142964c981c78fcba19166c597eca715826207
sanikani <111363766+sanikani@users.noreply.github.com>
2025-04-24 21:54:52 +0900

    [level 2] Title: 기능개발, Time: 10.62 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> answer = new ArrayList<>();
+        for(int i=0;i<progresses.length;i++){
+            int remain = 100 - progresses[i];
+            int time = remain/speeds[i];
+            if(remain%speeds[i]>0){
+                time++;
+            }
+            queue.offer(time);
+        }
+        
+        int i = queue.poll();
+        int count = 1;
+        //맨앞꺼내고 다음값보다 크면 count++, q.poll()
+        //작으면 answer.add(count), i = q.poll();
+        while(true){
+            if(queue.isEmpty()){
+                answer.add(count);
+                break;
+            }
+            if(queue.peek()>i){
+                answer.add(count);
+                count = 1;
+                i = queue.poll();
+            }else{
+                queue.poll();
+                count++;
+            }
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
