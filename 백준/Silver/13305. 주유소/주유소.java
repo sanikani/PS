dff6f3b98bbb81b96a7942292a0859fe5e48e902
sanikani <111363766+sanikani@users.noreply.github.com>
2025-11-20 22:09:10 +0900

    [Silver III] Title: 주유소, Time: 316 ms, Memory: 31016 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] length = new int[N-1];
+		int[] price = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N-1; i++) {
+			length[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N; i++) {
+			price[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int totalPrice = 0;
+		for(int i=0; i<N-1; i++) {
+			totalPrice += price[i]*length[i];
+			int curPrice = price[i];
+			int index = i+1;
+			
+			while(index<N-1) {
+				if (curPrice<price[index]) {
+					totalPrice += curPrice*length[index];
+					index++;
+					i++;
+				}else {
+					break;
+				}
+			}
+		}
+		System.out.println(totalPrice);
+	}
+
+}
