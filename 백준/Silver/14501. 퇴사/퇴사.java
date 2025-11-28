911a8ee756ebd7d08b5288b7eefc2332797c93c4
sanikani <111363766+sanikani@users.noreply.github.com>
2025-11-28 20:57:06 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -2,31 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    static int[][] reservation;
-    static int max;
-    static int N;
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        max = 0;
-        reservation = new int[N+1][2];
+        int N = Integer.parseInt(br.readLine());
+        int max = 0;
+        int[][] reservation = new int[N+1][2];
         for(int i=1; i<=N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             reservation[i][0] = Integer.parseInt(st.nextToken());
             reservation[i][1] = Integer.parseInt(st.nextToken());
         }
-        dfs(1,0);
-        System.out.println(max);
-    }
-    
-    private static void dfs(int day, int total){
-        if(day==N+1){
-            max = Math.max(max, total);
-        }else{
-            if(day+reservation[day][0]<=N+1){
-                dfs(day + reservation[day][0], total + reservation[day][1]);
+        int[] dp = new int[N+2];
+        for(int i=1; i<=N+1; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i]);
+
+            if (i <= N) {
+                int nextDay = i + reservation[i][0];
+                if (nextDay <= N+1) {
+                    dp[nextDay] = Math.max(dp[nextDay], dp[i]+reservation[i][1]);
+                }
             }
-            dfs(day + 1, total);
         }
+        
+        System.out.println(dp[N+1]);
     }
 }
