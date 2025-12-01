be2b2c7e9da6f9fc305281b82de2aa28c207761f
sanikani <111363766+sanikani@users.noreply.github.com>
2025-12-02 02:02:33 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 444 ms, Memory: 45064 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -7,20 +7,23 @@ public class Main{
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        Map<Integer, String> nameMap = new HashMap<>();
+        String[] nameMap = new String[N+1];
         Map<String, Integer> numberMap = new HashMap<>();
         for(int i=1; i<=N; i++){
             String name = br.readLine();
-            nameMap.put(i, name);
+            nameMap[i] = name;
             numberMap.put(name, i);
         }
+        
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<M; i++){
             String s = br.readLine();
             if(Character.isDigit(s.charAt(0))){
-                System.out.println(nameMap.get(Integer.parseInt(s)));
+                sb.append(nameMap[Integer.parseInt(s)]).append('\n');
             }else{
-                System.out.println(numberMap.get(s));
+                sb.append(numberMap.get(s)).append('\n');
             }
         }
+        System.out.print(sb.toString());
     }
 }
