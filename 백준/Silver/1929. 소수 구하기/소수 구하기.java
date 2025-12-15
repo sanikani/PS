85e7b690820955687e335af9e8b53ff941b00669
sanikani <111363766+sanikani@users.noreply.github.com>
2025-12-15 23:43:51 +0900

    [Silver III] Title: 소수 구하기, Time: 452 ms, Memory: 19040 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        for(int i=N; i<=M; i++){
+            if(isPrime(i)) sb.append(i).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+    
+    public static boolean isPrime(int n){
+        if(n<2) return false;
+        if(n==2) return true;
+        if(n%2==0) return false;
+        
+        for(int i=3; i*i<=n; i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
