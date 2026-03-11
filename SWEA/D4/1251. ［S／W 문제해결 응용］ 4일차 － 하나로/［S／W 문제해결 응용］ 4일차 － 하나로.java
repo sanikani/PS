3c30f9e82aff5fe8eecb05c9976106cb7f912e44
김산이 <111363766+sanikani@users.noreply.github.com>
2026-03-11 11:58:58 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 480 ms, Memory: 146,980 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -2,12 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int[] parent;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -15,7 +15,6 @@ public class Solution {
 		
 		for(int tc = 1; tc <= T; tc++) {
 			int n = Integer.parseInt(br.readLine());
-			makeSet(n);
 			int[][] pos = new int[n][2];
 			
 			for(int i = 0; i < 2; i++) {
@@ -26,61 +25,58 @@ public class Solution {
 			}
 			
 			double k = Double.parseDouble(br.readLine());
-			List<Edge> edges = new ArrayList<>();
+			List<List<Node>> nodes = new ArrayList<>();
+			for(int i = 0; i <= n; i++) nodes.add(new ArrayList<>());
 			
-			for (int i = 0; i < n; i++) {
-				for(int j = i + 1; j < n; j++) {
-					long w = (long) (Math.pow(Math.abs(pos[i][0] - pos[j][0]), 2)
-							+ Math.pow(Math.abs(pos[i][1] - pos[j][1]), 2));
-					edges.add(new Edge(i, j, w));
-				}
-			}
+			makeNodes(n, pos, nodes);
 			
-			Collections.sort(edges, (e1, e2) -> Long.compare(e1.w, e2.w));
-			
-			long cost = 0;
-			for (Edge edge : edges) {
-				if(find(edge.u) != find(edge.v)) {
-					union(edge.u, edge.v);
-					cost += edge.w;
-				}
-			}
-			
-			sb.append('#').append(tc).append(' ').append(Math.round(cost * k)).append('\n');
+			sb.append('#').append(tc).append(' ').append(Math.round(prim(n, nodes) * k)).append('\n');
 		}
 		System.out.println(sb);
 	}
 	
-	static class Edge{
-		int u;
+	static class Node{
 		int v;
 		long w;
 		
-		public Edge(int u, int v, long w) {
-			this.u = u;
+		public Node(int v, long w) {
 			this.v = v;
 			this.w = w;
 		}
 	}
-	
-	static void makeSet(int n) {
-		parent = new int[n+1];
-		
-		for(int i = 1; i <= n; i++) {
-			parent[i] = i;
+
+	private static void makeNodes(int n, int[][] pos, List<List<Node>> nodes) {
+		for (int i = 0; i < n; i++) {
+			for(int j = i + 1; j < n; j++) {
+				long w = (long) (Math.pow(Math.abs(pos[i][0] - pos[j][0]), 2)
+						+ Math.pow(Math.abs(pos[i][1] - pos[j][1]), 2));
+				nodes.get(i).add(new Node(j, w));
+				nodes.get(j).add(new Node(i, w));
+			}
 		}
 	}
-	
-	static int find(int n) {
-		if(parent[n] == n) return n;
+
+	private static long prim(int n, List<List<Node>> nodes) {
+		PriorityQueue<Node> pq = new PriorityQueue<>((e1, e2) -> Long.compare(e1.w, e2.w));
+		boolean[] visited = new boolean[n + 1];
+		pq.offer(new Node(1, 0));
+		long cost = 0;
+		int cnt = 0;
 		
-		return parent[n] = find(parent[n]);
-	}
-	
-	static void union(int a, int b) {
-		int pa = find(a);
-		int pb = find(b);
-		
-		if(pa != pb) parent[pb] = pa;
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			if(visited[cur.v]) continue;
+			
+			visited[cur.v] = true;
+			cost += cur.w;
+			cnt++;
+			
+			if(cnt == n) break;
+			
+			for (Node node : nodes.get(cur.v)) {
+				if(!visited[node.v]) pq.offer(new Node(node.v, node.w));
+			}
+		}
+		return cost;
 	}
 }
