e4a7137221a91c5723c39c8f1cb8fb2cca06147d
김산이 <111363766+sanikani@users.noreply.github.com>
2026-03-13 14:49:54 +0900

    [D4] Title: 중간값 구하기, Time: 1,101 ms, Memory: 130,248 KB -BaekjoonHub

---
+++ SWEA/D4/3000. 중간값 구하기/중간값 구하기.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static final int MOD = 20171109;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int A = Integer.parseInt(st.nextToken());
+			
+			PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
+			PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+			
+			maxHeap.offer(A);
+			long result = 0;
+			for(int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				if(maxHeap.peek() < a) {
+					minHeap.offer(a);
+				}else {
+					maxHeap.offer(a);
+				}
+				if(maxHeap.peek() < b) {
+					minHeap.offer(b);
+				}else {
+					maxHeap.offer(b);
+				}
+				while(minHeap.size() != maxHeap.size() - 1) {
+					if(minHeap.size() >= maxHeap.size()) {
+						maxHeap.offer(minHeap.poll());
+					}else {
+						minHeap.offer(maxHeap.poll());
+					}
+				}
+				result = (result + maxHeap.peek()) % MOD;
+			}
+			
+			sb.append('#').append(tc).append(' ').append(result).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
