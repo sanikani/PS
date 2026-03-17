723469605294eaf656e8bf7dcfd3abe52fd085b0
김산이 <111363766+sanikani@users.noreply.github.com>
2026-03-17 13:44:01 +0900

    [Silver I] Title: 스티커, Time: 644 ms, Memory: 113148 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int tc = 1; tc <= T; tc++) {
@@ -25,9 +26,8 @@ public class Main {
 				dp[1][i] = Math.max(dp[0][i - 1], dp[2][i - 1]) + arr[0][i - 1];
 				dp[2][i] = Math.max(dp[1][i - 1], dp[0][i - 1]) + arr[1][i - 1];
 			}
-			
-			System.out.println(Math.max(dp[0][n], Math.max(dp[1][n], dp[2][n])));
+			sb.append(Math.max(dp[0][n], Math.max(dp[1][n], dp[2][n]))).append('\n');
 		}
+		System.out.println(sb);
 	}
-
 }
