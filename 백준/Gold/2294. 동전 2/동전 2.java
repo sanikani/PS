447ada5e1ade7b0d243a1448fcb707867c3abdb5
김산이 <111363766+sanikani@users.noreply.github.com>
2026-03-18 16:25:00 +0900

    [Gold V] Title: 동전 2, Time: 120 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,22 +16,16 @@ public class Main {
 		}
 		
 		int[] dp = new int[k+1];
+		Arrays.fill(dp, 100001);
+		dp[0] = 0;
+        
 		for(int money = 1; money <= k; money++) {
 			for(int coin : coins) {
 				if(money - coin < 0) continue;
-				if(dp[money - coin] > 0) {
-					if(dp[money] != 0) {
-						dp[money] = Math.min(dp[money], dp[money - coin] + 1);
-					}else {
-						dp[money] = dp[money - coin] + 1;
-					}
-				}else {
-					if(money % coin != 0) continue;
-					dp[money] = money / coin;
-				}
+				dp[money] = Math.min(dp[money], dp[money - coin] + 1);
 			}
 		}
-		if(dp[k] == 0) System.out.println(-1);
+		if(dp[k] == 100001) System.out.println(-1);
 		else System.out.println(dp[k]);
 	}
 }
