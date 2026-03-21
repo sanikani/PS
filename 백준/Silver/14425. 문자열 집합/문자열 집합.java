f752cee5c94c274e5bb536aca8eb192d195705b0
김산이 <111363766+sanikani@users.noreply.github.com>
2026-03-22 01:43:45 +0900

    [Silver IV] Title: 문자열 집합, Time: 364 ms, Memory: 31276 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,59 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    static final char OFFSET = 'a';
-    static class Node{
-        boolean isEnd;
-        Node[] childs = new Node['z' - OFFSET + 1];
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        Node start = new Node();
-        //
+
+        HashSet<String> set = new HashSet<>();
+
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            Node cur = start;
-            int idx = 0;
-            while (idx < s.length()) {
-                Node child = cur.childs[s.charAt(idx) - OFFSET];
-                if (child == null) {
-                    cur.childs[s.charAt(idx) - OFFSET] = new Node();
-                    child = cur.childs[s.charAt(idx) - OFFSET];
-                }
-                cur = child;
-                idx++;
-            }
-            cur.isEnd = true;
+            set.add(br.readLine());
         }
 
         int cnt = 0;
+
         for (int i = 0; i < m; i++) {
-            String s = br.readLine();
-            if(isContains(s, start)) cnt++;
+            String query = br.readLine();
+            if (set.contains(query)) {
+                cnt++;
+            }
         }
 
         System.out.println(cnt);
-    }
-
-    static boolean isContains(String s, Node start) {
-        int idx = 0;
-        Node cur = start;
-        while (idx < s.length()) {
-            Node child = cur.childs[s.charAt(idx) - OFFSET];
-            if (child == null) {
-                return false;
-            }
-            cur = child;
-            idx++;
-        }
-        return cur.isEnd;
     }
 }
