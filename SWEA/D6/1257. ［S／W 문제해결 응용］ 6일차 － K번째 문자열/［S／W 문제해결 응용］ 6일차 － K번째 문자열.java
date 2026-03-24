1118e682ab89624e1e3183f38bb3cbbee7b9ba22
김산이 <111363766+sanikani@users.noreply.github.com>
2026-03-24 20:22:42 +0900

    [D6] Title: [S/W 문제해결 응용] 6일차 - K번째 문자열, Time: 197 ms, Memory: 80,808 KB -BaekjoonHub

--- SWEA/D6/1257. ［S／W 문제해결 응용］ 6일차 － K번째 문자열/［S／W 문제해결 응용］ 6일차 － K번째 문자열.java
+++ SWEA/D6/1257. ［S／W 문제해결 응용］ 6일차 － K번째 문자열/［S／W 문제해결 응용］ 6일차 － K번째 문자열.java
@@ -1,97 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-    static final char OFFSET = 'a';
-    static Node root;
-    static String s;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
+
         for (int tc = 1; tc <= T; tc++) {
             int k = Integer.parseInt(br.readLine());
-            s = br.readLine();
-            root = new Node();
+            String s = br.readLine();
+
+            TreeSet<String> set = new TreeSet<>();
 
             for (int i = 0; i < s.length(); i++) {
-                insert(i);
-            }
-
-            sb.append('#').append(tc).append(' ').append(findKth(k)).append('\n');
-        }
-        System.out.println(sb);
-    }
-
-    static class Node{
-        int cnt;
-        Node[] child;
-        boolean isWord;
-
-        public Node() {
-            cnt = 0;
-            child = new Node[26];
-            isWord = false;
-        }
-    }
-
-    static void insert(int start) {
-        Node cur = root;
-
-        for (int i = start; i < s.length(); i++) {
-            int idx = s.charAt(i) - OFFSET;
-
-            if (cur.child[idx] == null) {
-                cur.child[idx] = new Node();
-            }
-
-            cur = cur.child[idx];
-
-            if (!cur.isWord) {
-                cur.isWord = true;
-                increaseCount(start, i);
-            }
-        }
-    }
-
-    static void increaseCount(int start, int end) {
-        Node cur = root;
-        cur.cnt++;
-
-        for (int i = start; i <= end; i++) {
-            int idx = s.charAt(i) - OFFSET;
-            cur = cur.child[idx];
-            cur.cnt++;
-        }
-    }
-
-    static String findKth(int k) {
-        if (root.cnt < k) return "none";
-
-        StringBuilder sb = new StringBuilder();
-        Node cur = root;
-
-        while (true) {
-            for (int i = 0; i < 26; i++) {
-                if (cur.child[i] == null) continue;
-
-                Node next = cur.child[i];
-
-                if (next.cnt < k) {
-                    k -= next.cnt;
-                } else {
-                    sb.append((char) (i + OFFSET));
-                    cur = next;
-
-                    if (cur.isWord) {
-                        k--;
-                        if (k == 0) return sb.toString();
-                    }
-                    break;
+                StringBuilder sb = new StringBuilder();
+                for (int j = i; j < s.length(); j++) {
+                    sb.append(s.charAt(j));
+                    set.add(sb.toString());
                 }
             }
+
+            String result = "none";
+
+            if (set.size() >= k) {
+                int idx = 1;
+                for (String str : set) {
+                    if (idx == k) {
+                        result = str;
+                        break;
+                    }
+                    idx++;
+                }
+            }
+
+            System.out.println("#" + tc + " " + result);
         }
     }
 }
