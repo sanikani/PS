78196a435eb361cc54c37a7ddd43af8bf2ba8b6e
김산이 <111363766+sanikani@users.noreply.github.com>
2026-03-25 10:44:18 +0900

    [D5] Title: [S/W 문제해결 응용] 6일차 - K번째 접미어, Time: 100 ms, Memory: 27,520 KB -BaekjoonHub

--- SWEA/D5/1256. ［S／W 문제해결 응용］ 6일차 － K번째 접미어/［S／W 문제해결 응용］ 6일차 － K번째 접미어.java
+++ SWEA/D5/1256. ［S／W 문제해결 응용］ 6일차 － K번째 접미어/［S／W 문제해결 응용］ 6일차 － K번째 접미어.java
@@ -1,12 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.TreeSet;
 
 public class Solution {
 	static int k;
 	static String s;
-	static Node root;
-	final static char OFFSET = 'a';
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -14,73 +13,24 @@ public class Solution {
 		for(int tc = 1; tc <= T; tc++) {
 			k = Integer.parseInt(br.readLine());
 			s = br.readLine();
-			root = new Node();
+			TreeSet<String> set = new TreeSet<>();
 			for(int start = 0; start < s.length(); start++){
-				insert(start);
-			}
-			sb.append('#').append(tc).append(' ').append(findKth()).append('\n');
-		}
-		System.out.println(sb);
-	}
-	
-	static class Node{
-		int cnt;
-		Node[] child;
-		boolean isWord;
-		
-		public Node(){
-			child = new Node[26];
-		}
-	}
-	
-	private static void insert(int start) {
-		Node cur = root;
-		
-		for(int end = start; end < s.length(); end++) {
-			int idx = s.charAt(end) - OFFSET;
-			if(cur.child[idx] == null) {
-				cur.child[idx] = new Node();
+				set.add(s.substring(start, s.length()));
 			}
 			
-			cur = cur.child[idx];
-			
-		}
-		
-		if(!cur.isWord) {
-			cur.isWord = true;
-			count(start, s.length() - 1);
-		}
-	}
-
-	private static void count(int start, int end) {
-		Node cur = root;
-		cur.cnt++;
-		for(int k = start; k <= end; k++) {
-			int idx = s.charAt(k) - OFFSET;
-			cur = cur.child[idx];
-			cur.cnt++;
-		}
-	}
-	
-	static String findKth() {
-		StringBuilder sb = new StringBuilder();
-		if(k > s.length()) return "none";
-		
-		Node cur = root;
-		
-		while(true) {
-			for(int i = 0; i < 26; i++) {
-				if(cur.child[i] == null) continue;
-				if(cur.child[i].cnt < k) {
-					k -= cur.child[i].cnt;
-				}else {
-					cur = cur.child[i];
-					if(cur.isWord) k--;
-					sb.append((char)(i + OFFSET));
-					if(k == 0) return sb.toString();
-					break;
+			String result = "none";
+			if(set.size() >= k) {
+				int idx = 1;
+				for(String str : set) {
+					if(idx == k) {
+						result = str;
+						break;
+					}
+					idx++;
 				}
 			}
+			sb.append('#').append(tc).append(' ').append(result).append('\n');
 		}
+		System.out.println(sb);
 	}
 }
