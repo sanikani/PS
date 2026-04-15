4e9408964a92e7f61c3ed2c3c38f7e0b8b3852be
김산이 <111363766+sanikani@users.noreply.github.com>
2026-04-15 11:55:17 +0900

    [D4] Title: 사랑의 카운슬러, Time: 130 ms, Memory: 28,032 KB -BaekjoonHub

---
+++ SWEA/D4/1494. 사랑의 카운슬러/README.md
@@ -0,0 +1,15 @@
+# [D4] 사랑의 카운슬러 - 1494 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV2b_WPaAEIBBASw) 
+
+### 성능 요약
+
+메모리: 28,032 KB, 시간: 130 ms, 코드길이: 1,684 Bytes
+
+### 제출 일자
+
+2026-04-15 11:55
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    static int N;
+    static long[][] arr;
+    static long min;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            N = Integer.parseInt(br.readLine());
+            arr = new long[N][2];
+            min = Long.MAX_VALUE;
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                arr[i][0] = Long.parseLong(st.nextToken());
+                arr[i][1] = Long.parseLong(st.nextToken());
+            }
+            dfs(0, 0, 0, 0, 0);
+            sb.append('#').append(tc).append(' ').append(min).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(long xSum, long ySum, int idx, int plusCnt, int minusCnt) {
+        if (idx == N) {
+            min = Math.min(min, xSum * xSum + ySum * ySum);
+            return;
+        }
+
+        if (plusCnt >= N / 2) {
+            dfs(xSum - arr[idx][0], ySum - arr[idx][1], idx + 1, plusCnt, minusCnt + 1);
+        } else if (minusCnt >= N / 2) {
+            dfs(xSum + arr[idx][0], ySum + arr[idx][1], idx + 1, plusCnt + 1, minusCnt);
+        } else {
+            dfs(xSum - arr[idx][0], ySum - arr[idx][1], idx + 1, plusCnt, minusCnt + 1);
+            dfs(xSum + arr[idx][0], ySum + arr[idx][1], idx + 1, plusCnt + 1, minusCnt);
+        }
+    }
+}
