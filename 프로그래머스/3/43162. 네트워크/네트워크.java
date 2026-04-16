dd1f619c93f9a5f58e9f178cc4475bbff0dc1797
김산이 <111363766+sanikani@users.noreply.github.com>
2026-04-16 15:31:43 +0900

    [level 3] Title: 네트워크, Time: 0.44 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,30 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] computers) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i <= n; i++){
-            graph.add(new ArrayList<>());
-        }
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(computers[i][j] == 0 || i == j) continue;
-                graph.get(i).add(j);
-            }
-        }
-        
         int cnt = 0;
         boolean[] vis = new boolean[n];
         for(int i = 0; i < n; i++){
             if(vis[i]) continue;
             cnt++;
-            bfs(i, vis, graph);
+            bfs(i, vis, computers, n);
         }
         
         return cnt;
     }
     
-    static void bfs(int start, boolean[] vis, List<List<Integer>> graph){
+    static void bfs(int start, boolean[] vis, int[][] computers, int n){
         
         Queue<Integer> q = new ArrayDeque<>();
         q.offer(start);
@@ -34,10 +22,10 @@ class Solution {
         while(!q.isEmpty()){
             int cur = q.poll();
             
-            for(int next : graph.get(cur)){
-                if(vis[next]) continue;
-                vis[next] = true;
-                q.offer(next);
+            for(int i = 0; i < n; i++){
+                if(cur == i || vis[i] || computers[cur][i] == 0) continue;
+                vis[i] = true;
+                q.offer(i);
             }
         }
     }
