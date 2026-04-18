cb9b642e37c1d05fed1a577bc996fe2d015dce48
김산이 <111363766+sanikani@users.noreply.github.com>
2026-04-18 23:42:19 +0900

    [level 2] Title: 바이러스 파이프, Time: 11.83 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.java
@@ -0,0 +1,80 @@
+import java.util.*;
+class Solution {
+    static List<Integer>[][] graph;
+    static int K;
+    static int max;
+    static int N;
+    
+    public int solution(int n, int infection, int[][] edges, int k) {
+        int answer = 0;
+        K = k;
+        N = n;
+        graph = new ArrayList[n + 1][3];
+        max = 0;
+        
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j < 3; j++){
+                graph[i][j] = new ArrayList<>();
+            }
+        }
+        
+        for(int i = 0; i < n - 1; i++){
+            int start = edges[i][0];
+            int to = edges[i][1];
+            int type = edges[i][2];
+            
+            graph[start][type - 1].add(to);
+            graph[to][type - 1].add(start);
+        }
+        
+        dfs(0, Set.of(infection));
+        return max;
+    }
+    
+    public void dfs(int level, Set<Integer> infections){
+        if(level == K){
+            max = Math.max(max, infections.size());
+            return;
+        }
+        
+        
+        //infections 복사 후 감염시키고, 다음으로 진행
+        Set<Integer> A = applyInfection(infections, 0);
+        A.addAll(infections);
+        dfs(level + 1, A);
+        
+        Set<Integer> B = applyInfection(infections, 1);
+        B.addAll(infections);
+        dfs(level + 1, B);
+        
+        Set<Integer> C = applyInfection(infections, 2);
+        C.addAll(infections);
+        dfs(level + 1, C);
+    }
+    
+    public Set<Integer> applyInfection(Set<Integer> infections, int type){
+        Set<Integer> result = new HashSet<>();
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] vis = new boolean[N + 1];
+        for(int infection : infections){
+            q.offer(infection);
+            vis[infection] = true;
+        }
+        
+        
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            
+            for(int next : graph[cur][type]){
+                if(!vis[next]){
+                    q.offer(next);
+                    vis[next] = true;
+                    result.add(next);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
