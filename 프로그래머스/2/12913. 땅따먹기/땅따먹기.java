4ce47155c274712d5e899b99ab9301735840f0ae
김산이 <111363766+sanikani@users.noreply.github.com>
2026-04-20 17:34:19 +0900

    [level 2] Title: 땅따먹기, Time: 19.92 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,16 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        
+        for(int i = 1; i < land.length; i++){
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][0]));
+        }
+        
+        int len = land.length;
+        return Math.max(Math.max(land[len - 1][0], land[len - 1][1]) ,
+                        Math.max(land[len - 1][2], land[len - 1][3]));
+    }
+}
